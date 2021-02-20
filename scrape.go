9f9112c60afb0165a2ae7978f4744af776efdc1d
sprajjwal <shashwatprajjwal@gmail.com>
2021-02-19 16:48:24 -0800

    done

--- scrape.go
+++ scrape.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
 
 	"github.com/gocolly/colly"
 )
+
+type Crypto struct {
+	Name   string
+	Symbol string
+	Price  string
+}
 
 // main() contains code adapted from example found in Colly's docs:
 // http://go-colly.org/docs/examples/basic/
@@ -12,12 +20,53 @@ func main() {
 	// Instantiate default collector
 	c := colly.NewCollector()
 
-	// On every a element which has href attribute call callback
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-                link := e.Attr("href")
+	f, err := os.OpenFile("output.json", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0660)
+	if err != nil {
+		panic(err)
+	}
 
-		// Print link
-                fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+	defer f.Close()
+
+	if _, err = f.WriteString("["); err != nil {
+		panic(err)
+	}
+
+	// On every a element which has href attribute call callback
+	c.OnHTML("tr", func(e *colly.HTMLElement) {
+		name, symbol, price := "", "", ""
+
+		e.ForEach("td", func(i int, elem *colly.HTMLElement) {
+
+			if i == 3 {
+				price = elem.Text
+			} else if i == 2 {
+				elem.ForEach("p", func(i int, p *colly.HTMLElement) {
+					if i == 0 {
+						name = p.Text
+					}
+					if i == 1 {
+						symbol = p.Text
+					}
+				})
+			}
+		})
+
+		if name != "" {
+			coin := Crypto{
+				Name:   name,
+				Symbol: symbol,
+				Price:  price,
+			}
+			coinJSON, _ := json.MarshalIndent(coin, "", " ")
+			fmt.Println(string(coinJSON))
+
+			if _, err = f.WriteString(string(coinJSON)); err != nil {
+				panic(err)
+			}
+			if _, err = f.WriteString(","); err != nil {
+				panic(err)
+			}
+		}
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,5 +75,8 @@ func main() {
 	})
 
 	// Start scraping on https://hackerspaces.org
-	c.Visit("https://hackerspaces.org/")
+	c.Visit("https://coinmarketcap.com/")
+	if _, err = f.WriteString("]"); err != nil {
+		panic(err)
+	}
 }
